b523a6605807b08f56e0634cbb3cc325c5102202
Angel Torres <125419049+A4NG31@users.noreply.github.com>
2025-10-17 11:59:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -488,7 +488,7 @@ def main():
     st.sidebar.info(f"✅ Pandas {pd.__version__}")
     
     st.sidebar.header("💱 Validar otro peaje")
-    st.sidebar("""
+    st.sidebar.info("""
     <div class="ezytec-section">
         <h2 class="sub-header">HONDA</h2>
         <div class="ezytec-card">
